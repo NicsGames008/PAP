2f3da09bc43216870bfcd7c76f00490194346bb2
NicsGames008 <nicstarwars008@gmail.com>
2022-11-04 18:15:31 +0000

    Login feito

--- PAPArturNicolau/Assets/Script/APIScript/userSelect.cs
+++ PAPArturNicolau/Assets/Script/APIScript/userSelect.cs
@@ -1,5 +1,8 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem.Interactions;
+using UnityEngine.UIElements;
 
 public class userSelect : MonoBehaviour
 {
@@ -7,29 +10,76 @@ public class userSelect : MonoBehaviour
     string url = "http://localhost/API/userSelect.php";
     //Lista que guarda todos os valores da API
     public string[] userData;
+    //Caixa de email e passaword que o utilizador pos
+    public TMP_InputField insertPassword, insertEmail;
+    public GameObject background,errorUser, errorPassword;
 
-    // Start is called before the first frame update
-    IEnumerator Start()
+    //Metedo chamado quando o utilizador carrega no butão
+    public void Login()
     {
         //Executa a API
         WWW users = new WWW(url);
-        yield return users;
+        /////////////////////////////////////////////////Isto esta errado
+        return;
         //recebe todos os dados
         string userDataString = users.text;
         //Guarda na lista
         userData = userDataString.Split(';');
 
 
-        //print(GetValueData(userData[1], "password:"));
+        //tira 1 valor ao tamanhao do arrai para bater certo
+        int index = userData.Length - 1;
+
+        //passa por todo o tamanho do array para...
+        for (int i = 0; i < index; i++)
+        {
+            //ver se ha algum email com o input que o utilizador pos
+            if (GetValueData(userData[i], "email:") == insertEmail.text)
+            {
+                //e ve se a passe esta certa
+                if (GetValueData(userData[i], "password:") == insertPassword.text)
+                {
+                    //passa para a proxima fasse e acaba com a procura
+                    print("passou");
+                    errorUser.SetActive(false);
+                    errorPassword.SetActive(false);
+                    OperServerSelect();
+                    return;
+                }
+                //se a passe n estiver certa acaba com a procura e manda um mnsg de erro
+                else
+                {
+                    errorPassword.SetActive(true);
+                    print("pass n esta certa");
+                    return;
+                }
+            }
+        }
+        //Se nada foi encontrado manda uma mensaguem de erro
+        print("n ha conta");
+        errorUser.SetActive(true);
     }
 
+
+
+    //Pega um valor especifico da API
     string GetValueData(string data, string index)
     {
+        //Le o index pedido quando é chamado o metedo e vai ler ate onde etsa o index pedido
         string value = data.Substring(data.IndexOf(index) + index.Length);
+        //retira o trço de difição adicionada na API
         if (value.Contains("|"))
         {
             value = value.Remove(value.IndexOf("|"));
         }
+        //devolve o valor singular
         return value;
+    }
+
+
+    //Muda de janela
+    public void OperServerSelect()
+    {
+        background.transform.LeanMoveLocal(new Vector3(0, 0, 0), 0.7f).setEaseInBack().setIgnoreTimeScale(true);
     }
 }
